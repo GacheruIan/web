43c65ba9ffd2728c50ea669d00ea9af560f2cf29
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-08-13 00:10:11 +0300

    modify files

--- assets/css/index.js
+++ assets/css/index.js
@@ -4,16 +4,7 @@
     const hours = x.getHours();
     const minutes = x.getMinutes();
     const sec = x.getSeconds()
-     if (hours<10){
-        hours = "0" + hours;
-     }
-     if (hours<10){
-        minutes = "0" + minutes;
-     }
-     if (sec<10){
-        sec = "0" + sec;
-     }
-    
+
     
      document.getElementById("hours").innerHTML = hours + ":";
      document.getElementById("minutes").innerHTML = minutes + ":";
